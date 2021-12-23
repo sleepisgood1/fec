faa78569f646b88bdb71c5a5bbc07529fc982a3a
sleepisgood1 <jae1994krw@gmail.com>
2021-12-22 23:13:58 -0800

    Finish setting up server, Do a successful API request, Create config.js
    Server set up is mostly done. Created a controller to house all the functions, model to house all the API calls, config.js to house the API key, successfully ran nodemon with npm start and called the API through my server using postman to localhost:3000/relateditems/
    Also pulled from main before doing pull request to prevent conflict

--- server/controller/index.js
+++ server/controller/index.js
@@ -2,6 +2,8 @@
 // are we using databases to store our own info?? are we creating our own db??? is data living in our server?
 // - if we are -> need to import models
 
-module.exports = {
-  relatedItems: require('./relatedItems.js')
-}
+
+exports.relatedItems = require('./relatedItems.js')
+// module.exports = {
+//   relatedItems: require('./relatedItems.js')
+// }
