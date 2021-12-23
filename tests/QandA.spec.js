d0fc37bcfe4a2886c5f0fd1de90521c1c8f7ab6c
Julia Zheng <juliaxzheng@gmail.com>
2021-12-22 16:30:13 -0800

    add Enzyme

--- tests/QandA.spec.js
+++ tests/QandA.spec.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import TodoList from '../client/components/TodoList.jsx';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 describe('Q&A test suite', () => {
   it('My Q&A Test', () => {
       expect(true).toEqual(true);
--- tests/RelatedItems.spec.js
+++ tests/RelatedItems.spec.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import TodoList from '../client/components/TodoList.jsx';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 describe('Related Items test suite', () => {
   it('My Related Items Test', () => {
       expect(true).toEqual(true);
