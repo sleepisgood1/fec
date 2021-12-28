c561fb552767649fb55246a746b3ead2a92bfd84
Julia Zheng <juliaxzheng@gmail.com>
2021-12-28 09:21:41 -0800

    pass product associated with productId as props to child component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,44 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import ProductDetail from './ProductDetail/ProductDetail.jsx'
+import { useParams } from "react-router-dom";
 const axios = require('axios');
 const config = require('../../../server/config/config.js');
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
-import Initial from './Initial.jsx';
-
 
 export default function App() {
   const { id } = useParams();
+  const [productId, setProductId] = useState(id);
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      setLoading(true);
+      try {
+        console.log(id)
+        const response = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/${id}`, {
+          headers: {
+            'Authorization': config.API_KEY
+          }
+        });
+        setProduct(response.data);
+        //console.log(product);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
   return (
-   <div>{id}</div>
-  );
+   <div>
+     <ProductDetail
+       productId = {productId}
+       changeProductId = {setProductId}
+       product = {product}
+       changeProductId = {setProduct}
+     />
+   </div>
+  )
 }
--- client/src/components/ProductDetail/ProductDetail.jsx
+++ client/src/components/ProductDetail/ProductDetail.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+const axios = require('axios');
+const config = require('../../../../server/config/config.js');
+
 
 class ProductDetail extends React.Component {
  constructor(props) {
    super(props)
  }
 
+
 render() {
+  console.log(this.props.product)
   return (
-    <div></div>
+    <div>{this.props.productId}</div>
   )
 }
 }
