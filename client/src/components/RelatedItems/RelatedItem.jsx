8de8aa496725e3b3c3ab69d83a0a84ecade217b2
sleepisgood1 <jae1994krw@gmail.com>
2021-12-31 20:40:10 -0800

    Completed Related Item and Related Items basic components
    Got the two components to render basic data. img, product name, sale price/normal price, average rating.
    Still need to create an icon component that shows a comparison of current product and related product when the icon on the related product card is pressed. Also need to convert average rating to stars. Still need to show sale price in red and have regular nonsale price crossed out. Still need current outfits

---
+++ client/src/components/RelatedItems/RelatedItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import StarRating from './StarRating.jsx'
+
+function RelatedItem(props) {
+  const relatedItemSKUs = props.relatedItem.results
+  const reviewData = props.relatedItem.reviewMetadata
+  const productInfo = props.relatedItem.productInfo
+  const expandedProductName = '' + relatedItemSKUs[0].name + ' ' + productInfo.name
+  const price = relatedItemSKUs[0].sale_price ? relatedItemSKUs[0].sale_price : relatedItemSKUs[0].original_price
+  //gonna get an array of items
+  const rating = Object.entries(reviewData.ratings).reduce((previousValue, currentValue)=>{
+    var curTotal = Number(currentValue[0])*Number(currentValue[1])
+    return previousValue + curTotal
+  }, Number(0))
+  const numberOfRatings = Object.values(reviewData.ratings).reduce(
+    (previousValue, currentValue) =>{
+      return previousValue + Number(currentValue)
+    }, Number(0))
+  const averageRating = numberOfRatings ? rating/numberOfRatings : ""
+  const priceRender = <li><a>{relatedItemSKUs[0].original_price}</a><b>{relatedItemSKUs[0].sale_price}</b></li>
+  //technically onclick of the button, it should probably change the state of something in the top level? 
+  return (
+    <ul>
+      <img src={relatedItemSKUs[0].photos[0].thumbnail_url} onClick={()=>{
+        console.log('clicked!')
+        // props.history.push(props.relatedItem.results)
+        console.log(props.product)
+        }}/>
+        <li>{productInfo.category}</li>
+        <li>{expandedProductName}</li>
+        {relatedItemSKUs[0].sale_price ? priceRender : relatedItemSKUs[0].original_price}
+        <li>{averageRating}</li>
+        <StarRating/>
+        <button onClick={}/>
+    </ul>
+  )
+}
+export default RelatedItem;
---
+++ client/src/components/RelatedItems/StarRating.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function StarRating (props)=>{
+  return (
+    <div>
+    &#9733
+    </div>
+  )
+}
+export default StarRating;
